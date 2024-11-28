13140e52752b18f8f21c8b15e59ce8c69913085e
marlon-erler <viridian035@gmail.com>
2024-11-28 17:55:17 +0000

    setup electron window

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello, world");
+import { app, BrowserWindow } from "electron";
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 900,
+    height: 700
+  });
+  window.loadURL("https://google.com");
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
