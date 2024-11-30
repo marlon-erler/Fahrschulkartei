7b1c9bf1738a23a36ff61f6d74c763aca58cb6ef
marlon-erler <viridian035@gmail.com>
2024-11-30 16:03:11 +0000

    cleanup, updates, add methods for school data

--- Backend/Model/model.ts
+++ Backend/Model/model.ts
@@ -2,8 +2,10 @@ import Os from "os";
 import Path from "path";
 
 import {Database, Table} from "pfsdb";
+import {SchoolKeys} from "./keys";
 
 const BASE_PATH = Path.join(Os.homedir(), "Fahrschulkartei");
+const SCHOOL_ENTRY_ID = "main";
 
 export default class Model {
     database: Database;
@@ -24,5 +26,18 @@ export default class Model {
 	this.theoryClassTable = new Table("theory-classes", this.database);
 	this.theoryClassAttendanceTable = new Table("theory-class-attendances", this.database);
 	this.practicalClassTable = new Table("practical-clases", this.database);
+    }
+
+    /*
+     * School Data
+     */
+
+    async setSchoolData(key: keyof SchoolKeys, value: string): Promise<void> {
+	return await this.schoolDataTable.setFieldValuesForEntry(SCHOOL_ENTRY_ID, key, [value]);
+    }
+
+    async getSchoolData(key: keyof SchoolKeys): Promise<string|undefined> {
+	const values = await this.schoolDataTable.getValuesForField(SCHOOL_ENTRY_ID, key);
+	return values[0] ?? undefined;
     }
 }
