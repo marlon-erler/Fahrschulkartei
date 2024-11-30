c34b673f557324db804e326da852e446da0645b4
marlon-erler <viridian035@gmail.com>
2024-11-30 12:48:25 +0000

    define all keys

---
+++ Backend/Model/keys.ts
@@ -0,0 +1,89 @@
+/*
+ * General
+ */
+export enum SchoolKeys {
+    "name",
+    "street",
+    "building-number",
+    "zip-code",
+    "city",
+}
+
+export enum PricingChartKeys {
+    "fahrstunde-45min",
+    "ueberlandfahrt-45min",
+    "nachtfahrt-45min",
+    "praktische-unterweisung-45min",
+    "vorstellung-theoriepruefung",
+    "vorstellung-praktische-pruefung",
+    "vorstellung-teil-praktisch",
+    "vorstellung-teil-abfahrtkontrolle",
+    "vorstellung-teil-verbinden-trennen",
+    "lernmittel",
+    "mofa",
+}
+
+/*
+ * Classes
+ */
+export enum TheoryClassKeys {
+    "date",
+    "start-time",
+    "duration",
+    "type",
+    "unit",
+    "class",
+    "attendees",
+}
+
+export enum TheoryClassAttendeeKeys {
+    "student",
+    "class",
+    "signature",
+}
+
+export enum PracticalClassKeys {
+    "date",
+    "start-time",
+    "duration-main",
+    "duration-other",
+    "instructor",
+    "vehicle",
+    "class-type",
+    "student",
+    "signature",
+}
+
+/*
+ * Students
+ */
+export enum StudentKeys {
+    "last-name",
+    "first-name",
+    "index",
+    "birthname",
+    "address",
+    "date-of-birth",
+    "place-of-birth",
+    "phone",
+    "e-mail",
+    "citizenship",
+    "employer",
+    "list-number",
+    "license-number",
+    "end-probation",
+    "requirements",
+    "prices",
+    "classes-requested",
+    "classes-owned",
+    "partner-school",
+    "privacy-accepted",
+    "signature-accepted 0, 1 (yes, sign everytime), or 2 (reuse signature)",
+    "date-of-registration",
+}
+
+export enum StudentLegalRequirementKeys {
+    "description",
+    "issuer",
+    "date",
+}
