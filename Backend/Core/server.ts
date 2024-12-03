32f0678afd4d9a528c0dd2ef3bb4f234fe46a52d
marlon-erler <viridian035@gmail.com>
2024-12-03 12:50:17 +0000

    prepare session tracking

--- Backend/Core/server.ts
+++ Backend/Core/server.ts
@@ -2,10 +2,23 @@ import Express from "express";
 import Ws from "express-ws";
 import Model from "../Model/model";
 import {WebSocket} from "ws";
+import {Tables} from "../Model/keys";
+
+export interface ClientInfo {
+    loadedEntry: string;
+    loadedKeys: string[];
+}
+
+export interface ParsedMessage {
+    messageId: string;
+    methodName: string;
+}
 
 export default class Server {
     app: Ws.Application;
     model: Model;
+
+    tracker = new Map<WebSocket, ClientInfo>();
 
     constructor(model: Model) {
 	const wsInstance: Ws.Instance = Ws(Express());
@@ -32,7 +45,42 @@ export default class Server {
 	});
     }
 
-    handleWebSocketMessage(ws: WebSocket, message: string): void {
+    // message handling
+    private handleWebSocketMessage(ws: WebSocket, message: string): void {
+	try {
+	    const parsedMessage: any = JSON.parse(message);
+
+	    // ensure properties exist
+	    if (typeof parsedMessage.messageId != "string") {
+		throw "messageId not defined";
+	    } else if (typeof parsedMessage.methodName != "string") {
+		throw "methodName not defined";
+	    } 
+
+	    this.handleParsedMessage(ws, parsedMessage);
+	} catch (e) {
+	    console.error(`could not parse message "${message}"`, e);
+	}
+    }
+
+    private handleParsedMessage(ws: WebSocket, message: ParsedMessage): void {
+
+    }
+
+    // tracking
+    private updateTracking(ws: WebSocket, entry: string, keys: string[]): void {
+	const clientInfo = () => this.tracker.get(ws);
+
+	const currentInfo = clientInfo();
+	if (currentInfo == undefined || currentInfo.loadedEntry != entry) {
+	    const newInfo: ClientInfo = {
+		loadedEntry: entry,
+		loadedKeys: [],
+	    }
+	    this.tracker.set(ws, newInfo);
+	}
+
+	clientInfo()?.loadedKeys.push(...keys);
     }
 }
 
